2dc095ae5d02d09e812fcdc0d5bca95036c43768
Maximilian Garmatsch <mail@maximiliangarmatsch.com>
2020-04-09 23:38:50 +0200

    Add files via upload

---
+++ src/controller/user-aws.controller.ts
@@ -0,0 +1,20 @@
+import { BasicController } from "./basic.controller";
+import { UserAWSService } from "../service/user-aws.service";
+
+export class UserAWSController extends BasicController<any>{
+    private service: UserAWSService = new UserAWSService();
+
+    post() {
+        return this.service.getOne(JSON.parse(this.body))
+    }
+    put() {
+        return {message:'Not implemented yet'}
+    }
+    delete() {
+        return {message:'Not implemented yet'}
+    }
+    get() {
+        return {message:'Not implemented yet'}
+    }
+
+}
