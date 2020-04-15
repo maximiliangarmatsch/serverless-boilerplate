8c623f9dc799bcfb5b2514d64af2b2c0f2585c15
Maximilian Garmatsch <mail@maximiliangarmatsch.com>
2020-04-16 00:49:18 +0200

    Update user-aws.controller.ts

--- src/controller/user-aws.controller.ts
+++ src/controller/user-aws.controller.ts
@@ -5,16 +5,20 @@ export class UserAWSController extends BasicController<any>{
     private service: UserAWSService = new UserAWSService();
 
     post() {
-        return this.service.getOne(JSON.parse(this.body))
+        if (this.body.pass) {
+            return this.service.getOne(JSON.parse(this.body))
+        } else {
+            return this.service.save(JSON.parse(this.body))
+        }
     }
     put() {
-        return {message:'Not implemented yet'}
+        return { message: 'Not implemented yet' }
     }
     delete() {
-        return {message:'Not implemented yet'}
+        return { message: 'Not implemented yet' }
     }
     get() {
-        return {message:'Not implemented yet'}
+        return { message: 'Not implemented yet' }
     }
 
 }
